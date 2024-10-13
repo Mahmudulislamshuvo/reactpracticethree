1031324aa5cb53a66b6b574402cba9c270d46246
Mahmudul Islam <mahmudulislammern@gmail.com>
2024-10-13 22:30:11 +0600

    Reset btn functionality

--- src/Components/GameStart.jsx
+++ src/Components/GameStart.jsx
@@ -1,57 +1,72 @@
-import React, { useState } from "react";
-import diceone_1 from "../assets/Dice/dice_1.png";
-import diceone_2 from "../assets/Dice/dice_2.png";
-import diceone_3 from "../assets/Dice/dice_3.png";
-import diceone_4 from "../assets/Dice/dice_4.png";
-import diceone_5 from "../assets/Dice/dice_5.png";
-import diceone_6 from "../assets/Dice/dice_6.png";
+import React, { useEffect, useState } from "react";
 
 const GameStart = () => {
   const SelectArray = [1, 2, 3, 4, 5, 6];
   const [Isselect, setIsselect] = useState(null);
-  const [randomnumber, setrandomnumber] = useState();
-  const diceImages = [
-    diceone_1,
-    diceone_2,
-    diceone_3,
-    diceone_4,
-    diceone_5,
-    diceone_6,
-  ];
+  const [randomnumber, setrandomnumber] = useState(1);
+  const [diceValue, setdiceValue] = useState("");
+  const [score, setscore] = useState(1);
+  const [error, seterror] = useState("");
 
   // handle selection
-  const handleSelection = (i) => {
+  const handleSelection = (i, value) => {
     setIsselect(i);
+    setdiceValue(value);
+    seterror("");
   };
 
   // handle dice roll
 
   const RandomNmber = (min, max) => {
+    console.log(diceValue, Isselect);
+
+    if (Isselect === null) {
+      seterror(true);
+      return;
+    }
+    if (Isselect) {
+      seterror(false);
+    }
     setrandomnumber(Math.floor(Math.random() * (max - min) + min));
   };
 
-  const handleDice = () => {};
+  useEffect(() => {
+    // handle score
+    if (diceValue === randomnumber) {
+      setscore(score + 4);
+    } else {
+      setscore(score - 1);
+    }
+  }, [randomnumber]);
+
+  // handle reset btn
+  const HandleResetBtn = () => {
+    setscore(0);
+  };
 
   return (
     <>
       <div className="mt-[50px] mx-[90px]">
         <div className="flex justify-between items-center text-center">
-          <div className="text-center max-w-[200px]">
+          <div className="text-center w-[200px]">
             <p className="font-Popins font-medium text-[100px] leading-none">
-              0
+              {score}
             </p>
             <p className="font-Popins font-medium text-[24px] leading-none">
               Total Score
             </p>
           </div>
-          <div className="text-center">
+          <div className="text-center flex justify-end items-end flex-col w-full">
+            <div className="text-red-500 pb-2">
+              {error ? "You have to select one to play game" : ""}
+            </div>
             <div className="flex gap-x-5">
               {SelectArray.map((value, i) => (
                 <div
                   key={i}
                   className={`cursor-pointer text-[24px] font-bold font-Popins flex text-center items-center justify-center border-2 border-solid border-black h-[70px] w-[70px] rounded-xl
                     ${Isselect === i ? "bg-black text-white" : ""}`}
-                  onClick={() => handleSelection(i)}
+                  onClick={() => handleSelection(i, value)}
                 >
                   {value}
                 </div>
@@ -61,16 +76,18 @@ const GameStart = () => {
               Select Number
             </p>
           </div>
+          <div></div>
         </div>
         <div>
           <div className="flex flex-col items-center justify-center">
             <div
               className="flex flex-col justify-center items-center cursor-pointer"
-              onClick={() => RandomNmber(0, 6)}
+              onClick={() => RandomNmber(1, 7)}
             >
               <picture>
                 <img
-                  src={diceImages[randomnumber]}
+                  // src={diceImages[randomnumber]}
+                  src={`/src/assets/Dice/dice_${randomnumber}.png`}
                   alt={`diceone_${randomnumber}`}
                 />
               </picture>
@@ -78,11 +95,14 @@ const GameStart = () => {
                 Click on Dice to roll
               </h5>
             </div>
-            <div class="group relative inline-block rounded-md mt-6">
-              <button class="relative z-10 px-[60px] py-[10px] font-semibold font-Popins text-[16px] border border-solid border-black bg-transparent text-black transition-colors duration-300 rounded-md">
+            <div className="group relative inline-block rounded-md mt-6">
+              <button
+                className="relative z-10 px-[60px] py-[10px] font-semibold font-Popins text-[16px] border border-solid border-black bg-transparent text-black transition-colors duration-300 rounded-md"
+                onClick={HandleResetBtn}
+              >
                 Reset Score
               </button>
-              <div class="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-blue-400 to-transparent transform scale-x-0 group-hover:scale-x-100 origin-left transition-transform duration-500 rounded-md"></div>
+              <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-blue-400 to-transparent transform scale-x-0 group-hover:scale-x-100 origin-left transition-transform duration-500 rounded-md"></div>
             </div>
             <button className="mt-6 px-[62px] py-[10px] font-semibold font-Popins text-[16px] bg-black text-white rounded-md">
               Show Rules
